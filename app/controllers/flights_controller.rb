624de34097b5d37f82e26591f159ba6465113e56
alex boceno <alexboceno@hotmail.fr>
2016-12-05 19:29:13 +0100

    first deploy

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -8,7 +8,6 @@ class FlightsController < ApplicationController
     Skyscanner::Connection.apikey = "prtl6749387986743898559646983194"
     results = Skyscanner::Connection.browse_routes( :country => "GB", :currency => "GBP", :locale => "en-GB", :originPlace => "UK", :destinationPlace => "anywhere", :outboundPartialDate => "anytime", :inboundPartialDate => "anytime" )
 
-    raise
     # @flight = Flight.new()
   end
 
