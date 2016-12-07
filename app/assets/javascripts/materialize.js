ecd4e6b4384fd63d1abbc05927ff31b9b0724fd9
Mael donnard <mael.donnard@gmail.com>
2016-12-07 17:36:07 +0100

    sky animation

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -1,4 +1,11 @@
  $(document).ready(function() {
     $(".dropdown-button").dropdown();
     $('select').material_select();
+
+    $('.datepicker').pickadate({
+      selectMonths: true,
+      selectYears: true,
+      dateFormat: 'MM yy'
+    });
   });
+
