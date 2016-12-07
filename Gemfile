441b4f7cf1c6d502dc68f290ab657dcf59fd64ab
alex boceno <alexboceno@hotmail.fr>
2016-12-07 10:53:59 +0100

    work in progress

--- Gemfile
+++ Gemfile
@@ -15,7 +15,6 @@ gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
 gem 'materialize-sass'
-gem 'skyscanner'
 
 group :development, :test do
   gem 'binding_of_caller'
--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -6,8 +6,21 @@ class FlightsController < ApplicationController
 
   def index
     Skyscanner::Connection.apikey = "prtl6749387986743898559646983194"
-    results = Skyscanner::Connection.browse_routes( :country => "GB", :currency => "GBP", :locale => "en-GB", :originPlace => "UK", :destinationPlace => "anywhere", :outboundPartialDate => "anytime", :inboundPartialDate => "anytime" )
-    @results = results["Routes"]
+    results = Skyscanner::Connection.browse_grid( :country => "GB", :currency => "GBP", :locale => "en-GB", :originPlace => "LON", :destinationPlace => "JFK", :outboundPartialDate => "2016-12", :inboundPartialDate => "2016-12" )
+    @results = results
 
   end
 end
+
+# :country => 'GB',
+#         :currency => 'GBP',
+#         :locale => "en-GB",
+#         :originPlace => "UK",
+#         :destinationPlace => "anywhere",
+#         :outboundPartialDate => "anytime",
+# :inboundPartialDate => false
+
+
+
+result1['OriginLocation'] => LAX
+result1['DestinationLocation'] => JFK
