66d8e92135ec9f4d0e22d21e314603da25c234ca
Matthieu Le Berre <matthieu.leberre@gmail.com>
2016-12-07 18:01:21 +0100

    création des JDD et affichage des éléments dans la vue

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -9,6 +9,50 @@ class FlightsController < ApplicationController
     # results = Skyscanner::Connection.browse_routes( :country => "GB", :currency => "GBP", :locale => "en-GB", :originPlace => "UK", :destinationPlace => "anywhere", :outboundPartialDate => "anytime", :inboundPartialDate => "anytime" )
 
     # @flight = Flight.new()
+    @trips = generate_sample_trips
   end
 
+
+
+
+private
+
+  def generate_sample_trips
+    trips = []
+    10.times do
+      start_flight = Flight.new(
+        start_place:["CDG","DPS","JFK","AMS","LGE"].sample,
+        end_place:["CDG","DPS","JFK","AMS","LGE"].sample,
+        start_date: DateTime.now + rand(1..10).days,
+        end_date:DateTime.now + rand(1..10).days,
+        company: ["Air France","Luftansa", "Vueling"].sample,
+        flight_duration: [ "6H30" , "1H30", "12H40","7H00" ].sample
+      )
+
+      return_flight = Flight.new(
+        start_place:["CDG","DPS","JFK","AMS","LGE"].sample,
+        end_place:["CDG","DPS","JFK","AMS","LGE"].sample,
+        start_date: DateTime.now + rand(1..10).days,
+        end_date:DateTime.now + rand(1..10).days,
+        company: ["Air France","Luftansa", "Vueling"].sample,
+        flight_duration: [ "6H30" , "1H30", "12H40","7H00" ].sample
+      )
+
+      trip_price = [350, 600, 700, 1300].sample
+      currency = ["$","¥","€"].sample
+      global_start_trip_duration = rand(3..50)
+      global_return_trip_duration = rand(3..50)
+
+      trip = {
+        start_flight: start_flight,
+        return_flight: return_flight,
+        trip_price: trip_price,
+        currency: currency,
+        global_start_trip_duration: global_start_trip_duration,
+        global_return_trip_duration: global_return_trip_duration
+      }
+      trips << trip
+    end
+    return trips
+  end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,5 +1,5 @@
 class Flight
   include ActiveModel::Model
-  attr_accessor :start_place , :end_place
+  attr_accessor :start_place , :end_place, :start_date, :end_date, :flight_duration, :company
 
 end
