1cf714e85db2db45b7390ec9dd872188461d0abe
Mael donnard <mael.donnard@gmail.com>
2016-12-12 16:38:38 +0100

    add modil alex

--- config/initializers/sacs_ruby.rb
+++ config/initializers/sacs_ruby.rb
@@ -1,10 +1,11 @@
 SacsRuby.configure do |config|
-  config.user_id = ENV["SABRE_API_ID"] # User ID
-  config.group = 'DEVCENTER' # Group
-  config.domain = 'EXT' # Domain
-  config.client_secret = ENV["SABRE_API_SECRET"]  # Client Secret
-  config.environment = 'https://api.test.sabre.com' # Environment
-  config.token_strategy = :single # or :shared - see Token paragraph
+ config.user_id = ENV["SABRE_API_ID"] # User ID
+ config.group = 'DEVCENTER' # Group
+ config.domain = 'EXT' # Domain
+ config.client_secret = ENV["SABRE_API_SECRET"]  # Client Secret
+ config.environment = 'https://api.test.sabre.com' # Environment
+ config.token_strategy = :single # or :shared - see Token paragraph
 end
+
 
 
