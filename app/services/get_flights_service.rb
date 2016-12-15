c4995e4af3edb310500c60a9cbcf7dd15bcf5d44
Mael donnard <mael.donnard@gmail.com>
2016-12-15 09:48:05 +0100

    correction home bis

--- app/services/get_flights_service.rb
+++ app/services/get_flights_service.rb
@@ -26,7 +26,7 @@ class GetFlightsService
 
 
     Skyscanner::Connection.url  = "partners.api.skyscanner.net/apiservices"
-    Skyscanner::Connection.apikey = "ma072197419186221733566119557648"
+    Skyscanner::Connection.apikey = "prtl6749387986743898559646983194"
     Skyscanner::Connection.response_format = "ruby"
 
 
