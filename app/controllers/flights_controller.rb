ba8b428fb87fe9bcdb86ea4f5c3e401b97bf516b
remy lecostey <remy.lecostey@gmail.com>
2016-12-15 09:21:51 +0100

    Add tempo to API Skyscanner

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -57,7 +57,7 @@ private
       "Février 2017" => "28",
       "Mars 2017" => "31",
       "Avril 2017" => "30",
-      "Mai 2017 " => "31",
+      "Mai 2017" => "31",
       "Juin 2017" => "30",
       "Juillet 2017" => "31",
       "Aout 2017" => "31",
@@ -89,87 +89,87 @@ private
   end
 
   # Fake flights for testing
-  def generate_sample_trips
-    trips = []
-    10.times do
-      start_flight = Flight.new(
-        start_place:["CDG","DPS","JFK","AMS","LGE"].sample,
-        end_place:["CDG","DPS","JFK","AMS","LGE"].sample,
-        start_date: DateTime.now + rand(1..10).days,
-        end_date:DateTime.now + rand(11..15).days,
-        company: ["Air France","Luftansa", "Vueling"].sample,
-        flight_duration: [ "6H30" , "1H30", "12H40","7H00" ].sample
-      )
+  # def generate_sample_trips
+  #   trips = []
+  #   10.times do
+  #     start_flight = Flight.new(
+  #       start_place:["CDG","DPS","JFK","AMS","LGE"].sample,
+  #       end_place:["CDG","DPS","JFK","AMS","LGE"].sample,
+  #       start_date: DateTime.now + rand(1..10).days,
+  #       end_date:DateTime.now + rand(11..15).days,
+  #       company: ["Air France","Luftansa", "Vueling"].sample,
+  #       flight_duration: [ "6H30" , "1H30", "12H40","7H00" ].sample
+  #     )
 
-      return_flight = Flight.new(
-        start_place:["CDG","DPS","JFK","AMS","LGE"].sample,
-        end_place:["CDG","DPS","JFK","AMS","LGE"].sample,
-        start_date: DateTime.now + rand(16..20).days,
-        end_date:DateTime.now + rand(21..30).days,
-        company: ["Air France","Luftansa", "Vueling"].sample,
-        flight_duration: [ "6H30" , "1H30", "12H40","7H00" ].sample
-      )
+  #     return_flight = Flight.new(
+  #       start_place:["CDG","DPS","JFK","AMS","LGE"].sample,
+  #       end_place:["CDG","DPS","JFK","AMS","LGE"].sample,
+  #       start_date: DateTime.now + rand(16..20).days,
+  #       end_date:DateTime.now + rand(21..30).days,
+  #       company: ["Air France","Luftansa", "Vueling"].sample,
+  #       flight_duration: [ "6H30" , "1H30", "12H40","7H00" ].sample
+  #     )
 
-      trip_price = [350, 600, 700, 1300].sample
-      currency = ["$","¥","€"].sample
-      global_start_trip_duration = rand(3..50)
-      global_return_trip_duration = rand(3..50)
+  #     trip_price = [350, 600, 700, 1300].sample
+  #     currency = ["$","¥","€"].sample
+  #     global_start_trip_duration = rand(3..50)
+  #     global_return_trip_duration = rand(3..50)
 
-      trip = {
-        start_flight: start_flight,
-        return_flight: return_flight,
-        trip_price: trip_price,
-        currency: currency,
-        global_start_trip_duration: global_start_trip_duration,
-        global_return_trip_duration: global_return_trip_duration
-      }
-      trips << trip
-    end
-    return trips
-  end
+  #     trip = {
+  #       start_flight: start_flight,
+  #       return_flight: return_flight,
+  #       trip_price: trip_price,
+  #       currency: currency,
+  #       global_start_trip_duration: global_start_trip_duration,
+  #       global_return_trip_duration: global_return_trip_duration
+  #     }
+  #     trips << trip
+  #   end
+  #   return trips
+  # end
 
-  def sort_by_duration(results)
-  end
+  # def sort_by_duration(results)
+  # end
 
-  def date_params(date)
+  # def date_params(date)
 
-    last_day_hash = {
-      "Janvier 2017" => "31",
-      "Février 2017" => "28",
-      "Mars 2017" => "31",
-      "Avril" => "30",
-      "Mai" => "31",
-      "Juin" => "30",
-      "Juillet" => "31",
-      "Aout" => "31",
-      "Septembre" => "30",
-      "Octobre" => "31",
-      "Novembre" => "30",
-      "Decembre" => "31",
+  #   last_day_hash = {
+  #     "Janvier 2017" => "31",
+  #     "Février 2017" => "28",
+  #     "Mars 2017" => "31",
+  #     "Avril 2017" => "30",
+  #     "Mai 2017" => "31",
+  #     "Juin 2017" => "30",
+  #     "Juillet 2017" => "31",
+  #     "Aout 2017" => "31",
+  #     "Septembre" => "30",
+  #     "Octobre" => "31",
+  #     "Novembre" => "30",
+  #     "Decembre 2016" => "31",
 
-    };
+  #   };
 
 
 
-    month_hash = {
-      "Janvier 2017" => "01",
-      "Février 2017" => "02",
-      "Mars 2017" => "03",
-      "Avril" => "04",
-      "Mai" => "05",
-      "Juin" => "06",
-      "Juillet" => "07",
-      "Aout" => "08",
-      "Septembre" => "09",
-      "Octobre" => "10",
-      "Novembre" => "11",
-      "Decembre" => "12",
+  #   month_hash = {
+  #     "Janvier 2017" => "01",
+  #     "Février 2017" => "02",
+  #     "Mars 2017" => "03",
+  #     "Avril 2017" => "04",
+  #     "Mai 2017" => "05",
+  #     "Juin" => "06",
+  #     "Juillet" => "07",
+  #     "Aout" => "08",
+  #     "Septembre" => "09",
+  #     "Octobre" => "10",
+  #     "Novembre" => "11",
+  #     "Decembre" => "12",
 
-    }
+  #   }
 
-    results = ["2017" + "-" + month_hash[date] +"-" + last_day_hash[date],
-    "2017" + "-" + month_hash[date] + "-01"]
-  end
+    # results = ["2017" + "-" + month_hash[date] +"-" + last_day_hash[date],
+    # "2017" + "-" + month_hash[date] + "-01"]
+  # end
 end
 
 
